cfe43fa363451be7dfb3dbf68990a15efd02ba9b
Abshar777 <absharameen625@gmail.com>
2026-03-13 17:47:40 +0530

    done

--- src/db.ts
+++ src/db.ts
@@ -1,21 +1,23 @@
-import mongoose from 'mongoose';
-import bcrypt from 'bcryptjs';
-import { Admin } from './models/Admin';
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import { Admin } from "./models/Admin";
 
-const MONGO_URI = process.env.MONGO_URI || "mongodb://delta:123@82.25.109.155:27017/tlogic";
+const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/tlogic";
 
-console.log(MONGO_URI,"MONGO_URI");
+console.log(MONGO_URI, "MONGO_URI");
 export async function connectDB() {
-  await mongoose.connect(MONGO_URI);
-  console.log('✅ MongoDB connected');
+  await mongoose.connect(MONGO_URI, {
+    authSource: "admin",
+  });
+  console.log("✅ MongoDB connected");
   await seedAdmin();
 }
 
 async function seedAdmin() {
   const count = await Admin.countDocuments();
   if (count === 0) {
-    const passwordHash = await bcrypt.hash('admin123', 10);
-    await Admin.create({ username: 'admin', passwordHash });
-    console.log('✅ Default admin created: admin / admin123');
+    const passwordHash = await bcrypt.hash("admin123", 10);
+    await Admin.create({ username: "admin", passwordHash });
+    console.log("✅ Default admin created: admin / admin123");
   }
 }
